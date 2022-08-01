749cd234720c4de1739370e0a427a552f5653354
agustin <agustinvillagran98@gmail.com>
2022-07-31 23:57:23 -0300

    fecha employer

--- codigosSQL/arreglos.sql
+++ codigosSQL/arreglos.sql
@@ -1,5 +1,4 @@
 ALTER TABLE cuenta ADD COLUMN tipo_cuenta INTEGER NOT NULL;
 
 --Corregir el campo employee_hire_date de la tabla empleado con la fecha en formato YYYY-MM-DD
-SELECT CONVERT(VARCHAR, GETDATE(), 111) AS [YYYY/MM/DD]
-FROM employee_hire_date;
+UPDATE empleado SET employee_hire_date = substr(employee_hire_date, 7, 4) || '-' || substr(employee_hire_date, 4,2) || '-' || substr(employee_hire_date, 1,2)
